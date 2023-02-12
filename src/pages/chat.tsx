896524d8a924198dfe1908ecd6b700b913072d95
96368a <ncxxg@qq.com>
2023-02-13 00:04:21 +0800

    :beers:增加输入框自动高度及换行~

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,5 +1,22 @@
 export default function Chat() {
-    return (<div id="__next">
+
+    const textareaAutoHeight = (e: Event)=>{
+        let ee = e.target as HTMLTextAreaElement
+        // 计算高度
+        ee.style.height = "24px"        
+        ee.style.height = `${ee.scrollHeight}px`
+    }
+    const textaeraEnter = (e: KeyboardEvent)=>{
+        if(e.code == "Enter"){
+            console.log(e.returnValue);
+            if(!e.shiftKey){
+                e.returnValue = false
+            }
+        }
+        
+    }
+
+    return (
         <div class="overflow-hidden w-full h-full relative">
             <div class="flex h-full flex-1 flex-col md:pl-[260px]">
                 <main
@@ -97,10 +114,10 @@ export default function Chat() {
                                 </div>
                                 <div
                                     class="flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]">
-                                    <textarea tabindex="0" data-id="root"
+                                    <textarea tabindex="0" data-id="root" onInput={textareaAutoHeight} onKeyDown={textaeraEnter}
                                         style="max-height: 200px; height: 24px; overflow-y: hidden;" rows="1"
                                         placeholder=""
-                                        class="m-0 w-full resize-none border-0 bg-transparent p-0 pl-2 pr-7 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:pl-0"></textarea><button
+                                        class="outline-none m-0 w-full resize-none border-0 bg-transparent p-0 pl-2 pr-7 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:pl-0"></textarea><button
                                             class="absolute p-1 rounded-md text-gray-500 bottom-1.5 right-1 md:bottom-2.5 md:right-2 hover:bg-gray-100 dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent"><svg
                                                 stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24"
                                                 stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4 mr-1"
@@ -346,6 +363,5 @@ export default function Chat() {
                 </div>
             </div>
         </div>
-        <div class="absolute top-0 left-0 right-0 z-[2]"></div>
-    </div>)
+    )
 }
